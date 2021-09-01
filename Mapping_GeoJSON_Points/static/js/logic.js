5806b3a6549c90822bcc36ac2b8e336e9a4949b4
sbretag <84825189+sbretag@users.noreply.github.com>
2021-08-31 22:39:02 -0500

    Updating GeoJSON files

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,21 +2,35 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-  color: "yellow"
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+  }
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
